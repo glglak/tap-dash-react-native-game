e9e7df21a167d2741f4f6cfbf03d43cca069bcbd
glglak <karem.kamel@gmail.com>
2025-03-15 07:34:45 +0200

    Create ultra-simple obstacle - just a green box

--- components/Obstacle.js
+++ components/Obstacle.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// Lightweight Obstacle Component - more visible but still performant
+// Ultra-simple Obstacle - just a green box
 const Obstacle = (props) => {
-  if (!props || !props.position || !props.size) {
+  const { position, size, hit } = props || {};
+  
+  if (!position || !size) {
     return null;
   }
   
-  const { position, size, hit } = props;
-  
-  // Very simplified obstacle - just a colored rectangle with details for visibility
   return (
     <View
       style={{
@@ -18,45 +17,11 @@ const Obstacle = (props) => {
         top: position.y - size.height / 2,
         width: size.width,
         height: size.height,
-        backgroundColor: hit ? '#FF6347' : '#228B22', // Red if hit, green otherwise
-        borderRadius: 3,
-        borderWidth: 3, // Thicker border for visibility
-        borderColor: '#000',
-        // Add spikes for visibility
-        alignItems: 'center',
-        justifyContent: 'flex-start'
+        backgroundColor: hit ? '#FF0000' : '#00FF00',
+        borderWidth: 2,
+        borderColor: '#000000'
       }}
-    >
-      {/* Simple "spikes" on top of obstacle */}
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        position: 'absolute',
-        top: -10
-      }}>
-        <View style={{
-          width: 0,
-          height: 0,
-          borderLeftWidth: 6,
-          borderRightWidth: 6,
-          borderBottomWidth: 10,
-          borderLeftColor: 'transparent',
-          borderRightColor: 'transparent',
-          borderBottomColor: hit ? '#FF0000' : '#006400'
-        }} />
-        <View style={{
-          width: 0,
-          height: 0,
-          borderLeftWidth: 6,
-          borderRightWidth: 6,
-          borderBottomWidth: 10,
-          borderLeftColor: 'transparent',
-          borderRightColor: 'transparent',
-          borderBottomColor: hit ? '#FF0000' : '#006400'
-        }} />
-      </View>
-    </View>
+    />
   );
 };
 
