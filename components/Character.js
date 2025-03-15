4468fe845c7a3ef681b08747fc054c05e637db8e
glglak <karem.kamel@gmail.com>
2025-03-15 07:34:37 +0200

    Create ultra-simple character - just a red box

--- components/Character.js
+++ components/Character.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 
-// Improved Character Component - more visible but still lightweight
+// Ultra-simple Character - just a red box
 const Character = (props) => {
-  if (!props || !props.position || !props.size) {
+  const { position, size } = props || {};
+  
+  if (!position || !size) {
     return null;
   }
-  
-  const { position, size, jumping } = props;
-  
-  // Bright colors for visibility
-  const characterColor = jumping ? '#FF0000' : '#FF0000'; // Always bright red
   
   return (
     <View
@@ -20,43 +17,11 @@ const Character = (props) => {
         top: position.y - size.height / 2,
         width: size.width,
         height: size.height,
-        backgroundColor: characterColor,
-        borderRadius: 8,
-        borderWidth: 3, // Thicker border for visibility
-        borderColor: '#000',
-        // Simple face to make it more visible
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: '#FF0000',
+        borderWidth: 2,
+        borderColor: '#000000'
       }}
-    >
-      {/* Simple eyes */}
-      <View style={{
-        width: size.width * 0.7,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-      }}>
-        <View style={{
-          width: 10,
-          height: 10,
-          backgroundColor: 'white',
-          borderRadius: 5,
-        }} />
-        <View style={{
-          width: 10,
-          height: 10,
-          backgroundColor: 'white',
-          borderRadius: 5,
-        }} />
-      </View>
-      
-      {/* Simple mouth */}
-      <View style={{
-        marginTop: 5,
-        width: size.width * 0.5,
-        height: 3,
-        backgroundColor: 'white',
-      }} />
-    </View>
+    />
   );
 };
 
