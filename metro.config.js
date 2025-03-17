5f75f63a1581fcc81d50d8c0bea3ecb777ab0d2e
glglak <karem.kamel@gmail.com>
2025-03-17 08:52:12 +0200

    Update metro.config.js to use newer React Native format

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,18 @@
-const { getDefaultConfig } = require('expo/metro-config');
+const {getDefaultConfig} = require('@react-native/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+// Additional configuration for Expo projects
+const { resolver: { sourceExts, assetExts } } = config;
+
+// Add additional extensions used by Expo and React Native
+config.resolver.assetExts = assetExts.filter(ext => ext !== 'svg');
+config.resolver.sourceExts = [...sourceExts, 'svg', 'mjs', 'cjs'];
+
+// Handle SVG files appropriately
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer')
+};
 
 module.exports = config;
